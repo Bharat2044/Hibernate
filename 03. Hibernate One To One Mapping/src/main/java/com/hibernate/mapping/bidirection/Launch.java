8f66590c94a45aed89fc50691ec6617110886c33
Bharat2044 <bk9955204456@gmail.com>
2024-05-06 15:10:32 +0530

    code added

---
+++ 03. Hibernate One To One Mapping/src/main/java/com/hibernate/mapping/bidirection/Launch.java
@@ -0,0 +1,83 @@
+package com.hibernate.mapping.bidirection;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class Launch {
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration();
+		cfg.configure();
+		
+		cfg.addAnnotatedClass(Customer.class);
+		cfg.addAnnotatedClass(CustomerDetails.class);
+		
+		Session session = cfg.buildSessionFactory().openSession();
+		Transaction t = session.beginTransaction();
+		
+		// Create
+		Customer c1 = new Customer(1, "Tim");
+		CustomerDetails cd1 = new CustomerDetails(101, "tim@gmail.com", 9900880099L, "Banglore");
+		c1.setCustomerDetails(cd1);
+		
+		// session.save(c1);
+		// session.persist(c1);
+		
+		Customer c2 = new Customer(2, "Mike");
+		CustomerDetails cd2 = new CustomerDetails(102, "mike@gmail.com", 7777880099L, "New York");
+		c2.setCustomerDetails(cd2);
+		// session.persist(c2);
+		
+		// Read
+		/*
+		int id = 2;
+		Customer c = session.get(Customer.class, id);
+		CustomerDetails cd = c.getCustomerDetails();		
+		*/
+
+
+		CustomerDetails cd = session.get(CustomerDetails.class, 101);
+		Customer c = cd.getCustomer();
+		
+		if(c != null) {
+			/*
+			System.out.print(c.getId() + "  ");
+			System.out.print(c.getName() + "  ");
+			System.out.print(cd.getId() + "  ");
+			System.out.print(cd.getEmail() + "  ");
+			System.out.print(cd.getPhone() + "  ");
+			System.out.print(cd.getAddress() + "  ");
+			*/
+			// System.out.println(c.toString());
+			System.out.println(cd.toString());
+		} else {
+			System.out.println("Custome is not present with this Id!!");
+		}
+		
+		Query<Customer> query = session.createQuery("FROM Customer c", Customer.class);
+		List<Customer> customers = query.getResultList();
+		
+		if(!customers.isEmpty()) {
+			for (Customer customer : customers) {
+				System.out.println(customer.toString());
+			}
+		} else {
+			System.out.println("No Customers Found!!");
+		}
+		
+		
+		// Delete
+		if(c != null) {
+			// session.remove(c);
+			// session.remove(cd);
+		} else {
+			System.out.println("Custome is not present with this Id!!");
+		}
+
+		t.commit();
+	}
+}
