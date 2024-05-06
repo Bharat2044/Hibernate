3373a90f42d178c1c47c11ead96aa9acc81ef389
Bharat2044 <bk9955204456@gmail.com>
2024-05-06 15:09:30 +0530

    code added

---
+++ 01. Hibernate Configuration/src/main/java/com/bharat/Employee.java
@@ -0,0 +1,81 @@
+package com.bharat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "salary")
+	private double salary;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "dept")
+	private String dept;
+	
+	public Employee() {
+
+	}
+
+	public Employee(int id, String name, double salary, String email, String dept) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.email = email;
+		this.dept = dept;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+}
